89329b924eff1ce4d274e402d146e6e17f8593c8
Kyara2005 <kyara.altamirano@epn.edu.ec>
2025-07-03 22:51:31 -0500

    te toca el css

--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -1,9 +1,25 @@
+import './Projects.css';
+
+import vibeuImg from '../../assets/logo-vibe-u.png'; // Asegúrate de tener esta imagen en tu carpeta assets
+
 const Projects = () => {
     return (
-        <section id="portfolio">
-            <h2>Proyectos</h2>
-            <p>Aquí se listarán los proyectos desarrollados por el grupo.</p>
-            <p><strong><a href="https://vibe-u.netlify.app/" target="_blank">https://vibe-u.netlify.app/</a></strong></p>
+        <section className="project" id="portfolio">
+            <h3 className="project__title">Proyectos</h3>
+            <p className="project__intro">Aquí se listarán los proyectos desarrollados por el grupo.</p>
+            <div className="project__cards">
+                <div className="cards">
+                    <img src={vibeuImg} className="card__img" alt="Proyecto Vibe-U" />
+                    <div className="card">
+                        <h4 className="card__title">Vibe-U</h4>
+                        <p className="card__description">Plataforma que pone a la universidad en modo social.</p>
+                        <div className="card__buttons">
+                            <a href="https://github.com/Kyara2005/Vibe-U" target="_blank" className="card__category">Repository</a>
+                            <a href="https://vibe-u.netlify.app/" target="_blank" className="card__category">Demo</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </section>
     );
 };
