93da6a4b89909f2d9648e21c97cca2b7223c7631
santyxxx-xxx <santiago.vargas@epn.edu.ec>
2025-07-10 17:47:40 -0500

    Update About.jsx

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,11 +1,26 @@
+import './About.css';
+import girl from '../../assets/girl.svg'; // le subes la imagen tu
+
 const About = () => {
     return (
-        <section id="bout">
-            <h2>Sobre el grupo</h2>
-            <p>Somos un equipo multidisciplinario enfocado en la creación de soluciones tecnológicas innovadoras.
-                Nuestra misión es impulsar el desarrollo digital con herramientas creativas que conecten a las personas. Nos especializamos en el diseño de aplicaciones web centradas en mejorar la experiencia social y académica.
-                Creemos en la innovación, el trabajo en equipo y el impacto positivo de la tecnología.
-            </p>
+        <section id="about" className="about">
+            <div className="about__content">
+                <h2 className="about__title">Sobre el grupo</h2>
+                <p className="about__paragraph">
+                    Somos un equipo multidisciplinario enfocado en la creación de soluciones tecnológicas innovadoras.
+                </p>
+                <p className="about__paragraph">
+                    Nuestra misión es impulsar el desarrollo digital con herramientas creativas que conecten a las personas. Nos especializamos en el diseño de aplicaciones web centradas en mejorar la experiencia social y académica.
+                </p>
+                <p className="about__paragraph">
+                    Creemos en la innovación, el trabajo en equipo y el impacto positivo de la tecnología.
+                </p>
+                <a href="#" className="btn btn__light">Ver más</a>
+            </div>
+
+            <figure className="about__figure">
+                <img src={girl} className="about__picture" alt="chica-code" />
+            </figure>
         </section>
     );
 };
