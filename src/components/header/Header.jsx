b2c4ee78cd7b559dd23a9d8c1b07d41eaaadde44
Kyara2005 <kyara.altamirano@epn.edu.ec>
2025-07-17 19:53:02 -0500

    _redicts

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -10,10 +10,10 @@ const Header = () => {
                 <p class="navbar__logo">Grupo 1</p>
                 <ul class="navbar__menu">
                     <li><a href="#navbar">Inicio</a></li>
-                    <li><a href="#bout">Sobre nosotros</a></li>
+                    <li><a href="#about">Sobre nosotros</a></li>
                     <li><a href="#portfolio">Proyectos</a></li>
                     <li><Link to="/habilidades">Habilidades</Link></li>
-                    <li><Link to="/contacto">Contactos</Link></li>
+                    <li><Link to="/contacto">Contacto</Link></li>                   
                     <li><Link to="/login">Ingresar</Link></li>
                     <li><Link to="/register">Registrarse</Link></li>
                 </ul>
